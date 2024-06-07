e44cfc6d71a7faa48b1c54fc84e416c2669b2538
Murilo Perosa <perosamurilo@gmail.com>
2024-06-07 08:22:04 -0300

    feat: Playning with structs.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/structs-go
+
+go 1.22.3
---
+++ structs.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	appUser := user{
+		firstName: getUserData("Please enter your first name: "),
+		lastName:  getUserData("Please enter your last name: "),
+		birthdate: getUserData("Please enter your birthdate (MM/DD/YYYY): "),
+		createdAt: time.Now(),
+	}
+
+	outputUserData(appUser)
+}
+
+func outputUserData(u user) {
+	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
